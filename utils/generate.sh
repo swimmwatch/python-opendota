c2bf0479de041a6eb2294ed383b7cfa4aea723e2
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-26 23:22:34 +0300

    Add specificatin path parameter

--- utils/generate.sh
+++ utils/generate.sh
@@ -3,10 +3,11 @@
 # Generate SDK
 
 PACKAGE_VERSION="$1"
+SPEC_PATH="$2"
 
 docker run --rm \
   -v "${PWD}":/local openapitools/openapi-generator-cli generate \
-  --input-spec https://api.opendota.com/api \
+  --input-spec "$SPEC_PATH" \
   --generator-name python \
   --template-dir /local/custom-template \
   --ignore-file-override /local/.openapi-generator-ignore \
