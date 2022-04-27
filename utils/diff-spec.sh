5c6441ccf22af11f2271e7a43ac57b17b7a4c7d1
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-04-27 15:08:53 +0300

    Add markdown path parameter

--- utils/diff-spec.sh
+++ utils/diff-spec.sh
@@ -5,4 +5,4 @@
 OLD_SPEC="$1"
 NEW_SPEC="$2"
 
-docker run --rm -v "$(pwd):/specs:ro" openapitools/openapi-diff:2.0.1 "$OLD_SPEC" "$NEW_SPEC"
+docker run --rm -v "$(pwd):/specs:ro" openapitools/openapi-diff:2.0.1 "$OLD_SPEC" "$NEW_SPEC" --markdown changes.md
